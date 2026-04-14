b3befc848579412b15c0aec89315dfc0ea79caf3
tanmaynimbalkar854-netizen <tanmaynimbalkar854@gmail.com>
2026-04-14 16:40:52 +0530

    Create loader.tsx

---
+++ src/loader.tsx
@@ -0,0 +1,10 @@
+export default function Loader() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+      <div className="w-16 h-16 border-4 border-purple-500 border-t-transparent rounded-full animate-spin mb-4"></div>
+      <h1 className="text-xl font-semibold animate-pulse">
+        Inspire Flow
+      </h1>
+    </div>
+  );
+}
