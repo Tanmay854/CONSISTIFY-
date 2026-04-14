4553cc369fe5e7bdd7491f3fd954a014bb16be19
tanmaynimbalkar854-netizen <tanmaynimbalkar854@gmail.com>
2026-04-14 16:42:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
 
-const queryClient = new QueryClient();
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loader />;
+
+  return (
+    <div>
+      {/* Your existing app code here */}
+    </div>
+  );
+}
 
 export default App;
